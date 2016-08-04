f6dbecb800c5656c5f6fa3828fecde57ee3b14e8
phanikumarp <phanikumarp@users.noreply.github.com>
2016-08-04 14:32:28 +0530

    Update SpringMain.java

--- src/main/java/com/ppk/SpringMain.java
+++ src/main/java/com/ppk/SpringMain.java
@@ -44,6 +44,7 @@ public class SpringMain {
 		crunchifyList.add("Paypal");
 		crunchifyList.add("Facebook");
 		crunchifyList.add("Yahoo");
+		
  	return "This is List method to Phani: \n "+ crunchifyList;
     	
     }
